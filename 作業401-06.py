8176c88c0edf7ec597112b1b622e98f155fb9309
winshow0515 <61896293+winshow0515@users.noreply.github.com>
2020-04-22 09:18:00 +0800

    第六堂課的作業

---
+++ 作業401-06.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Apr 18 09:59:01 2020
+
+@author: p35k
+"""
+
+from mcpi.minecraft import Minecraft
+mc=Minecraft.create()
+while True:
+    hits = mc.events.pollProjectileHits()
+    if len(hits)>0:
+        hit=hits[0]
+        x,y,z=hit.pos.x, hit.pos.y, hit.pos.z
+        mc.player.setTilePos(x,y,z)
+        mc.spawnEntity(x,y,z,99)
