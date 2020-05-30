456cb3789f819ce70ff696820bd458b1a57984ee
winshow0515 <61896293+winshow0515@users.noreply.github.com>
2020-05-30 12:16:57 +0800

    第十一堂的作業

---
+++ 作業401-11.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat May 23 10:00:32 2020
+
+@author: p35k
+"""
+
+from mcpi.minecraft import Minecraft
+mc=Minecraft.create()
+from random import randrange
+
+x,y,z=mc.player.getTilePos()
+t=x
+for i in range(10):
+    x=x+i
+    
+    for j in range(10):
+        r=randrange(0,16)
+        z=z+1
+        mc.setBlock(x,y,z,35,r)
+    x=t
